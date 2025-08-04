82922ed52825903bf77cc036a1f99ac7f9835d2e
estudner000 <estudner000@warren.k12.in.us>
2025-08-04 13:24:36 -0400

    Initial commit 2

--- src/Bio.java
+++ src/Bio.java
@@ -8,20 +8,15 @@ public class Bio {
         System.out.print("Enter your name: ");
         String name = input.nextLine();
 
-        // TODO: prompt for the age.
         System.out.print("Enter your age: ");
         int age = input.nextInt();
-        // TODO: use input.nextInt() to start age.
 
-        // TODO: prompt for GPA.
         System.out.print("Enter your G.P.A: ");
         double gpa = input.nextDouble();
 
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
         System.out.println("G.P.A: " + gpa);
-        // TODO: repeat for age
-        // TODO: repeate for gpa.
 
         int gpaTruncated = (int) gpa;
 
